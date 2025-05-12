45881dc7240a98808b8258f81191223b98d9940e
Nguyen Hong Nhan <nhnhanit@gmail.com>
2025-05-12 16:17:05 +0700

    Message: - Create new project

---
+++ SwiftUIKitPatterns/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftUIKitPatterns
+//
+//  Created by hongnhan on 12/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        titleLabel.text = "Swift UIKit Patterns Hello!"
+    }
+
+
+}
+
---
+++ SwiftUIKitPatternsTests/SwiftUIKitPatternsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKitPatternsTests.swift
+//  SwiftUIKitPatternsTests
+//
+//  Created by hongnhan on 12/5/25.
+//
+
+import Testing
+@testable import SwiftUIKitPatterns
+
+struct SwiftUIKitPatternsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
